73e0859108a5bc48c9de426ece803d1e4fc3d414
Golden Ogbeka <golden@getrova.com>
2026-03-06 12:32:02 +0100

    removed script files

--- cleanup-md.js
+++
@@ -1,5 +0,0 @@
-// This file was created to clean up .md files but couldn't be executed due to environment issues
-// The following files should be manually deleted:
-// - API_FIXES.md
-// - COMPREHENSIVE_API_ANALYSIS.md  
-// - DATA_STRUCTURE_FIXES.md
--- install-deps.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-echo "Installing lucide-react..."
-npm install lucide-react@^0.468.0
-echo "Installation complete!"
